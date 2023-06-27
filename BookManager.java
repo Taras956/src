cd684cf273dc128ccd74fea7b5dbf9e6cd5fa007
Taras956 <119292144+Taras956@users.noreply.github.com>
2023-06-27 17:22:03 +0300

    Update BookManager.java

--- BookManager.java
+++ BookManager.java
@@ -1,62 +1,91 @@
-package ua.lviv.iot.algo.part1.book;
+class Book:
+    def __init__(self, title, author, publisher, year, genre):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        self.genre = genre
 
-import java.util.ArrayList;
-import java.util.List;
+    def get_title(self):
+        return self.title
 
-public class BookManager {
-    private List<Book> books;
+    def get_author(self):
+        return self.author
 
-    public BookManager() {
-        this.books = new ArrayList<Book>();
-    }
+    def get_publisher(self):
+        return self.publisher
 
-    public void addBook(Book book) {
-        this.books.add(book);
-    }
-    public List<Book> searchByAuthor(String author) {
-        List<Book> result = new ArrayList<>();
-        for (Book book : books) {
-            if (book.getAuthor().equals(author)) {
-                result.add(book);
-            }
-        }
-        return result;
-    }
-    
-    public List<Book> searchByGenre(String genre) {
-        List<Book> result = new ArrayList<>();
-        for (Book book : books) {
-            if (book.getGenre().equals(genre)) {
-                result.add(book);
-            }
-        }
-        return result;
-    }
-    
+    def get_year(self):
+        return self.year
 
-    public static void main(String[] args) {
-        List<Book> books = new ArrayList<>();
-        books.add(new PaperBook("Harry Poter", "Lviv tratata", "T.Tupytsia", 2036, "Romantic", 349, 150, 228));
-        books.add(new ElectronicBook("Harry Poter", "Lviv tratata", "T.Tupytsia", 2036, "Romantic", "PDF", 1024 * 1024));
+    def get_genre(self):
+        return self.genre
 
-        for (Book book : books) {
-            System.out.println(book);
-        }
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Publisher: {self.publisher}, Year: {self.year}, Genre: {self.genre}"
 
-        BookManager bookManager = new BookManager();
-        Book book1 = new Book("Harry Potter", "J.K. Rowling", "Bloomsbury Publishing", 1997, "Fantasy");
-        Book book2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", "George Allen & Unwin", 1954, "Fantasy");
-        bookManager.addBook(book1);
-        bookManager.addBook(book2);
-        List<Book> booksByAuthor = bookManager.searchByAuthor("J.K. Rowling");
-List<Book> booksByGenre = bookManager.searchByGenre("Fantasy");
 
-for (Book book : booksByAuthor) {
-    System.out.println(book.getTitle() + " by " + book.getAuthor());
-}
+class PaperBook(Book):
+    def __init__(self, title, author, publisher, year, genre, pageCount, weight, isbn):
+        super().__init__(title, author, publisher, year, genre)
+        self.page_count = pageCount
+        self.weight = weight
+        self.isbn = isbn
 
-for (Book book : booksByGenre) {
-    System.out.println(book.getTitle() + " is a " + book.getGenre() + " book");
-}
-    }
-}
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Publisher: {self.publisher}, Year: {self.year}, Genre: {self.genre}, Page Count: {self.page_count}, Weight: {self.weight}, ISBN: {self.isbn}"
+
+
+class ElectronicBook(Book):
+    def __init__(self, title, author, publisher, year, genre, format, fileSize):
+        super().__init__(title, author, publisher, year, genre)
+        self.format = format
+        self.file_size = fileSize
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Publisher: {self.publisher}, Year: {self.year}, Genre: {self.genre}, Format: {self.format}, File Size: {self.file_size}"
+
+
+class BookManager:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def search_by_author(self, author):
+        result = []
+        for book in self.books:
+            if book.get_author() == author:
+                result.append(book)
+        return result
+
+    def search_by_genre(self, genre):
+        result = []
+        for book in self.books:
+            if book.get_genre() == genre:
+                result.append(book)
+        return result
+
+
+books = []
+books.append(PaperBook("Harry Poter", "Lviv tratata", "T.Tupytsia", 2036, "Romantic", 349, 150, 228))
+books.append(ElectronicBook("Harry Poter", "Lviv tratata", "T.Tupytsia", 2036, "Romantic", "PDF", 1024 * 1024))
+
+for book in books:
+    print(book)
+
+book_manager = BookManager()
+book1 = Book("Harry Potter", "J.K. Rowling", "Bloomsbury Publishing", 1997, "Fantasy")
+book2 = Book("The Lord of the Rings", "J.R.R. Tolkien", "George Allen & Unwin", 1954, "Fantasy")
+book_manager.add_book(book1)
+book_manager.add_book(book2)
+
+books_by_author = book_manager.search_by_author("J.K. Rowling")
+books_by_genre = book_manager.search_by_genre("Fantasy")
+
+for book in books_by_author:
+    print(book.get_title(), "by", book.get_author())
+
+for book in books_by_genre:
+    print(book.get_title(), "is a", book.get_genre(), "book")
